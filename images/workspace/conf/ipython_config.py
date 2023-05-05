bf414770c3c462750b2c8b97d212b81ed5dd44ac
Gabriel <gabrielpalma92@gmail.com>
2023-05-05 00:07:37 -0300

    add kafka producer

---
+++ images/workspace/conf/ipython_config.py
@@ -0,0 +1,32 @@
+# Get the config
+c = get_config()
+
+# Pre-load the sparksql+trino magic
+c.InteractiveShellApp.extensions = [
+    'jupyterlab_sql_editor.ipython_magic_trino','jupyterlab_sql_editor.ipython_magic_sparksql'
+        ]
+
+# Pre-configure the sparksql magic
+
+c.SparkSql.limit=20
+c.SparkSql.cacheTTL=3600
+c.SparkSql.outputFile='/tmp/sparkdb.schema.json'
+c.SparkSql.catalogs='default'
+
+# Pre-configure trino magic
+
+import trino
+#c.Trino.auth=trino.auth.BasicAuthentication("admin", "admin")
+c.Trino.auth=None
+c.Trino.user='root'
+# host = trino container name
+c.Trino.host='trino'
+c.Trino.port=8080
+c.Trino.httpScheme='http'
+c.Trino.cacheTTL=3600
+c.Trino.outputFile='/tmp/trinodb.schema.json'
+# comma-separated list of catalogs
+c.Trino.catalogs="system,tpch,hive"
+
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_noode_interactivity = 'all'
--- images/workspace/conf/jupyter_notebook_config.py
+++ images/workspace/conf/jupyter_notebook_config.py
@@ -512,7 +512,7 @@ c.NotebookApp.notebook_dir = 'usr/local/spark_dev/work'
 #          Setting to an empty string disables authentication altogether, which
 #  is NOT RECOMMENDED.
 #  Default: '<generated>'
-# c.NotebookApp.token = '<generated>'
+#c.NotebookApp.token = ''
 
 ## Supply overrides for the tornado.web.Application that the Jupyter notebook
 #  uses.
